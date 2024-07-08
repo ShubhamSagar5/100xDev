15735dd3c870a17bbf338a5078a364e502680e07
shubham <shubhamsagar628@gmail.com>
2024-07-08 20:08:42 +0530

    Configure Tailwind

---
+++ week8/Tailwind/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3">
+      <div className="bg-red-800 ">1</div>
+      <div className="bg-green-700 ">2</div>
+      <div className="bg-blue-700 ">3</div>
+    </div>
+  );
+}
+
+export default App;
